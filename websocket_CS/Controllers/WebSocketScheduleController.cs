e119b4e7cd81e61dc4d286237c5679c8e2400279
shisojuice <>
2024-05-15 16:01:05 +0900

    作業中：schedule機能

---
+++ websocket_CS/Controllers/WebSocketScheduleController.cs
@@ -0,0 +1,125 @@
+using System.Net.WebSockets;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace websocket_CS.Controllers;
+
+public class ScheduleDto
+{
+    public string id { get; set; } = "";
+    public decimal x { get; set; } = 0;
+    public decimal y { get; set; } = 0;
+    public decimal z { get; set; } = 1;//zindex
+    public decimal w { get; set; } = 0;//width
+    public string bkc { get; set; } = "green";
+    public string rowId { get; set; } = "";
+    public decimal from { get; set; } = 0;
+    public decimal to { get; set; } = 0;
+    public string value { get; set; } = "";
+}
+public class rowDto
+{
+    public string rowId { get; set; } = "";
+    public int rowOrder { get; set; } = 0;
+    public string name { get; set; } = "";
+}
+public class BoardDto
+{
+    public string gridhtml { get; set; } = "";
+    public List<rowDto> rows { get; set; } = new List<rowDto>();
+    public string objhtml { get; set; } = "";
+    public List<ScheduleDto> logs { get; set; } = new List<ScheduleDto>();
+}
+public class WebSocketScheduleController : ControllerBase
+{
+    public static List<ScheduleDto> pastLogs =
+        new List<ScheduleDto>() {
+            new ScheduleDto() { id = "obj1", x = 235, y = 110, z = 1, w = 90, bkc = "green" , rowId = "rowId0" , from = 0, to = 0, value = "test1" },
+            new ScheduleDto() { id = "obj2", x = 295, y = 140, z = 1, w = 90, bkc = "green" , rowId = "rowId1" , from = 0, to = 0, value = "test2"  },
+            new ScheduleDto() { id = "obj3", x = 355, y = 170, z = 1, w = 90, bkc = "green" , rowId = "rowId2" , from = 0, to = 0, value = "test3"  }
+        };
+    public static List<rowDto> defrows =
+    new List<rowDto>() {
+            new rowDto() { rowId = "rowId0" , rowOrder = 0 , name = "aaa"  },
+            new rowDto() { rowId = "rowId1" , rowOrder = 1 , name = "bbb"  },
+            new rowDto() { rowId = "rowId2" , rowOrder = 2 , name = "ccc"  },
+            new rowDto() { rowId = "rowId3" , rowOrder = 3 , name = "ddd"  },
+            new rowDto() { rowId = "rowId4" , rowOrder = 4 , name = "eee"  },
+            new rowDto() { rowId = "rowId5" , rowOrder = 5 , name = "fff"  },
+            new rowDto() { rowId = "rowId6" , rowOrder = 6 , name = "ggg"  },
+            new rowDto() { rowId = "rowId7" , rowOrder = 7 , name = "hhh"  }
+    };
+
+    [HttpGet("/api/schedules")]
+    public BoardDto GetMessages()
+    {
+        BoardDto board = new BoardDto();
+        board.rows = defrows;
+        board.logs = pastLogs;
+        string rowhtml = "";
+        for (int i = 0; i < board.rows.Count; i++)
+        {
+            rowhtml = rowhtml + @"<div data-rowId=" + board.rows[i].rowId + @">" + board.rows[i].name + @"</div>";
+        }
+        board.gridhtml = board.gridhtml + @"<div class=""grid-rowcontainer"" style=""grid-template-rows: repeat(" + board.rows.Count + @", 1fr);"" >" + rowhtml + @"</div>";
+        string cellhtml = "";
+        for (int i = 0; i < board.rows.Count * 24; i++)
+        {
+            cellhtml = cellhtml + "<div></div>";
+        }
+        board.gridhtml = board.gridhtml + @"<div class=""grid-maincontainer"" style=""grid-template-rows: repeat(" + board.rows.Count + @", 1fr);"" >" + cellhtml + @"</div>";
+
+        for (int i = 0; i < board.logs.Count; i++)
+        {
+            var log = board.logs[i];
+            string attr = " id=" + log.id
+                        + @" style=""height=20px;width:" + log.w + "px; position:absolute;left:" + log.x + "px;top:" + log.y + "px;z-index:" + log.z + @";background-color:" + log.bkc + @";"""
+                        + " value=" + log.value
+                        + " data-rowid=" + log.rowId
+                        + " title=" + strTime(log.from) + "`" + strTime(log.to) + "_" + log.value;
+            board.objhtml = board.objhtml + @"<input" + attr + "></input>";
+        }
+        return board;
+    }
+
+    [Route("/ws_schedules")]
+    public async Task Get()
+    {
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            WebSocketManager.AddSocket(webSocket);
+            await Echo(webSocket);
+            WebSocketManager.RemoveSocket(webSocket);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+
+
+    private static async Task Echo(WebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4];
+        var receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        while (!receiveResult.CloseStatus.HasValue)
+        {
+            string utf8String = Encoding.UTF8.GetString(buffer, 0, receiveResult.Count);
+            //pastLogs.Add(utf8String);
+            await WebSocketManager.BroadcastAsync(utf8String);
+
+            receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        }
+
+        await webSocket.CloseAsync(receiveResult.CloseStatus.Value, receiveResult.CloseStatusDescription, CancellationToken.None);
+    }
+    private static string strTime(decimal time)
+    {
+        string ret = time.ToString().PadLeft(4, '0');
+        return ret.Substring(0, 2) + ":" + ret.Substring(2, 2);
+    }
+
+}
---
+++ websocket_CS/Controllers/WebSocketScheduleManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+namespace websocket_CS.Controllers
+{
+    public class WebSocketScheduleManager
+    {
+        //重複排除のためHashSet
+        private static readonly HashSet<WebSocket> _sockets = new HashSet<WebSocket>();
+
+        public static void AddSocket(WebSocket socket)
+        {
+            _sockets.Add(socket);
+        }
+
+        public static void RemoveSocket(WebSocket socket)
+        {
+            _sockets.Remove(socket);
+        }
+        public static async Task InitBroadcastAsync(WebSocket socket, List<string> messages, CancellationToken ct = default)
+        {
+            foreach (var message in messages)
+            {
+                var data = Encoding.UTF8.GetBytes(message);
+                var segment = new ArraySegment<byte>(data);
+                if (socket.State == WebSocketState.Open)
+                {
+                    await socket.SendAsync(segment, WebSocketMessageType.Text, true, ct);
+                }
+                else
+                {
+                    RemoveSocket(socket);
+                }
+            }
+        }
+        public static async Task BroadcastAsync(string message, CancellationToken ct = default)
+        {
+            var data = Encoding.UTF8.GetBytes(message);
+            var segment = new ArraySegment<byte>(data);
+
+            foreach (var socket in _sockets.ToList())
+            {
+                if (socket.State == WebSocketState.Open)
+                {
+                    await socket.SendAsync(segment, WebSocketMessageType.Text, true, ct);
+                }
+                else
+                {
+                    RemoveSocket(socket);
+                }
+            }
+        }
+    }
+}
